76dfc98506f6de06655d69fa91d239fee1dacfb2
yorberth01 <yorberthquiroz1@gmail.com>
2022-05-25 19:06:05 -0400

    se agrego la libreria jquery para poder pre-visualizar las imagenes antes de subirlas al servidor

--- sistema/includes/scripts.php
+++ sistema/includes/scripts.php
@@ -1,3 +1,4 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/functions.js"></script>
 	<?php include "functions.php"; ?>
